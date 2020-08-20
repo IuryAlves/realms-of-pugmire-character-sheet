20ec59e1b2bf11af8513abd1f73aa240712423db
IuryAlves <iuryalves20@gmail.com>
2020-08-20 21:25:23 +0200

    Add parameter --development to build templates for development

--- mustache.js
+++ mustache.js
@@ -7,37 +7,43 @@ var path = require('path');
 
 
 var args = process.argv;
-var baseTemplate = args[2];
-var output = args[3];
+var environment = args[2]
+var baseTemplate = args[3];
+var output = args[4];
 var view = {
+  isDevelopment: environment === "--development",
+  isProduction: environment === "--production",
   attributes_list: [
-  {
-    attribute_name: "str",
-    mod_field_name: "attr_stre_mod"
-  },
-  {
-    attribute_name: "dex",
-    mod_field_name: "attr_dex_mod"
-  },
-  {
-    attribute_name: "con",
-    mod_field_name: "attr_cont_mod"
-  },
-  {
-    attribute_name: "int",
-    mod_field_name: "attr_int_mod"
-  },
-  {
-    attribute_name: "wis",
-    mod_field_name: "attr_wis_mod"
-  },
-  {
-    attribute_name: "cha",
-    mod_field_name: "attr_cha_mod"
-  }
-]
+    {
+      attribute_name: "str",
+      mod_field_name: "attr_stre_mod"
+    },
+    {
+      attribute_name: "dex",
+      mod_field_name: "attr_dex_mod"
+    },
+    {
+      attribute_name: "con",
+      mod_field_name: "attr_cont_mod"
+    },
+    {
+      attribute_name: "int",
+      mod_field_name: "attr_int_mod"
+    },
+    {
+      attribute_name: "wis",
+      mod_field_name: "attr_wis_mod"
+    },
+    {
+      attribute_name: "cha",
+      mod_field_name: "attr_cha_mod"
+    }
+  ]
+
 };
+
 var partials = {};
+
 var customTags = ['<%', '%>'];
 var templateDir = __dirname + '/templates/';
 
@@ -46,11 +52,21 @@ var readTemplate = function(templateName, encoding) {
 };
 
 fs.readdirSync(templateDir).forEach(function(filename) {
-	partial_name = filename.split('.')[0];
-	partials[partial_name] = readTemplate(filename);
+  	partial_name = filename.split('.')[0];
+    var template = readTemplate(filename);
+    partials[partial_name] = template;
 });
 
-var template = fs.readFileSync(__dirname + path.sep + baseTemplate, 'utf-8')
-var html = mustache.render(template, view, partials, customTags);
-
-fs.writeFileSync(output, html, 'utf-8')
+if (view.isProduction){
+  var template = fs.readFileSync(__dirname + path.sep + baseTemplate, 'utf-8')
+  var html = mustache.render(template, view, partials, customTags);
+  fs.writeFileSync(output, html, 'utf-8')
+} else {
+  fs.readdirSync(templateDir).forEach(function(filename) {
+    out_filename = filename.split('.')[0] + '.html'
+    console.log('Generating file: ' + out_filename);
+    var template = readTemplate(filename);
+    var html = mustache.render(template, view, partials, customTags);
+    fs.writeFileSync(__dirname + '/devel/' + out_filename, html, 'utf-8');
+  });
+}
