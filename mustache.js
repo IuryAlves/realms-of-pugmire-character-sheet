d098f437b1f498bfebb868ffdf71155380183531
IuryAlves <iuryalves20@gmail.com>
2020-08-23 14:42:26 +0200

    Create mustache template for css

--- mustache.js
+++ mustache.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs');
 var mustache = require('mustache');
+var glob = require('glob');
 var process = require('process');
 var path = require('path');
 
@@ -9,7 +10,8 @@ var path = require('path');
 var args = process.argv;
 var environment = args[2]
 var baseTemplate = args[3];
-var output = args[4];
+var baseCss = args[4]
+var output = args[5];
 var view = {
   isDevelopment: environment === "--development",
   isProduction: environment === "--production",
@@ -43,13 +45,16 @@ var view = {
 };
 
 var partials = {};
+var css_partials = {};
 
 var customTags = ['<%', '%>'];
 var templateDir = __dirname + '/templates/';
+var cssDir = __dirname + '/css/';
 
-var readTemplate = function(templateName, encoding) {
+var readTemplate = function(templateName) {
 	return fs.readFileSync(templateDir + templateName, 'utf-8');
 };
+
 
 fs.readdirSync(templateDir).forEach(function(filename) {
   	partial_name = filename.split('.')[0];
@@ -57,11 +62,22 @@ fs.readdirSync(templateDir).forEach(function(filename) {
     partials[partial_name] = template;
 });
 
+glob.sync(cssDir + '**/*.css').forEach(function(file_path) {
+  filename = file_path.split('/').pop()
+  partial_name = filename.split('.')[0] + '-css';
+  var css = fs.readFileSync(file_path, 'utf-8');
+  css_partials[partial_name] = css;
+});
+
 if (view.isProduction){
   var template = fs.readFileSync(__dirname + path.sep + baseTemplate, 'utf-8')
+  var cssTemplate = fs.readFileSync(__dirname + path.sep + baseCss, 'utf-8')
   var html = mustache.render(template, view, partials, customTags);
+  var css = mustache.render(cssTemplate, {} , css_partials, customTags);
   fs.writeFileSync(output, html, 'utf-8')
-} else {
+  fs.writeFileSync('dist/realms-of-pugmire.css', css, 'utf-8')
+} 
+else {
   fs.readdirSync(templateDir).forEach(function(filename) {
     out_filename = filename.split('.')[0] + '.html'
     console.log('Generating file: ' + out_filename);
